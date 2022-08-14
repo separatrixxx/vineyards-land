8b97e3972514bbe0e0ae16eb769e3c20275a424f
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-08-15 00:51:10 +0300

    Add files via upload

--- src/components/MainForms.js
+++ src/components/MainForms.js
@@ -4,17 +4,17 @@ import React from 'react';
 function MainForms () {
     return (
         <div className="flex justify-center w-full p-5 mt-10 md:mt-0 drop-shadow-xl">
-            <div className="flex flex-col justify-center items-center w-full md:w-2/3 lg:w-1/3 bg-white rounded-3xl p-5">
-                <div className="flex justify-center pt-7">
+            <div className="flex flex-col justify-center items-center w-full md:w-2/3 lg:w-1/3 bg-white rounded-3xl px-5">
+                <div className="flex justify-center mt-7">
                     <h1 id="login_title" className="text-white text-sm md:text-lg bg-green-600 px-5 py-1 scale-110
                     rounded-full cursor-pointer transition-all duration-500 ease-in-out mr-1 md:mr-2">Авторизация</h1>
                     <h1 id="registration_title" className="text-black text-sm md:text-lg hover:bg-black/10 px-5 py-1
                     rounded-full cursor-pointer transition-all duration-500 ease-in-out ml-1 md:ml-2">Регистрация</h1>
                 </div>
-                <div className="w-5/6 h-px bg-slate-200 mt-7"></div>
-                <div id="login_form" className="flex flex-col items-center">
+                <div className="w-5/6 h-px bg-slate-200 my-7"></div>
+                <div id="login_form" className="flex flex-col items-center pb-7">
                     <input
-                        className="block shadow-inner md:w-96 h-10 w-64 mt-7 rounded-full pl-8 p-3 outline-green-600
+                        className="block shadow-inner md:w-96 h-10 w-64 rounded-full pl-8 p-3 outline-green-600
                             focus:outline-2 ease-in-out duration-300"
                         placeholder="E-mail" type="text"/>
                     <input
@@ -28,9 +28,9 @@ function MainForms () {
                         Войти
                     </button>
                 </div>
-                <div id="registration_form" className="flex flex-col items-center pt-0 hidden">
+                <div id="registration_form" className="flex flex-col items-center pb-7 hidden">
                     <input
-                        className="block shadow-inner md:w-96 h-10 w-64 mt-7 rounded-full pl-8 p-3 outline-green-600
+                        className="block shadow-inner md:w-96 h-10 w-64 rounded-full pl-8 p-3 outline-green-600
                             focus:outline-2 ease-in-out duration-300"
                         placeholder="E-mail" type="text"/>
                     <input
@@ -46,9 +46,9 @@ function MainForms () {
                         Зарегистрироваться
                     </button>
                 </div>
-                <div id="forgot_password_form" className="flex flex-col items-center pt-0 hidden">
+                <div id="forgot_password_form" className="flex flex-col items-center pb-7 hidden">
                     <input
-                        className="block shadow-inner md:w-96 h-10 w-64 mt-7 rounded-full pl-8 p-3 outline-green-600
+                        className="block shadow-inner md:w-96 h-10 w-64 rounded-full pl-8 p-3 outline-green-600
                             focus:outline-2 ease-in-out duration-300"
                         placeholder="E-mail" type="text"/>
                     <button id="forgot_password_btn" className="md:w-48 h-10 w-36 rounded-full mt-10 md:mt-14 text-xs md:text-sm bg-green-600
