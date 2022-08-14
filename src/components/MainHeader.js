22b3a28e9cf8d8a39d4656721e3506007a112720
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-08-14 22:59:20 +0300

    Update MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,7 +6,7 @@ function MainHeader () {
     return (
         <div className="p-2 md:p-5">
             <a href="/" className="flex items-center w-max py-3 rounded-full cursor-pointer hover:bg-black/10
-        transition-colors duration-300 ease-in-out">
+        transition-colors duration-500 ease-in-out">
                 <img className="w-16 md:w-20 select-none pr-2 md:pr-3 pl-4 md:pl-6" draggable="false" src={logo} alt="logo_icon" />
                 <h1 className="text-white text-xl md:text-2xl pl-2 md:pl-3 pr-4 md:pr-6">Vineyard's Land</h1>
             </a>
